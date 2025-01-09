4512378ffebda7b037376ef0ef290e6111f91261
Benjamin-kodehode <benjamin2.kodehode@gmail.com>
2025-01-09 13:45:01 +0100

    START

---
+++ index.js
@@ -0,0 +1,2 @@
+import { mine_sweeper } from "mine-sweeper-tag";
+customElements.define("mine-sweeper", mine_sweeper);
---
+++ index2.js
@@ -0,0 +1,3 @@
+import Game from "./node_modules/four-in-a-row/src/game.js";
+export * from "./node_modules/four-in-a-row/src/constants.js";
+export { Game };
